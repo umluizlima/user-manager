bd8961cf61a9d19e9577abee1e3ff60e373d6500
Luiz Lima <umluizlima@gmail.com>
2020-06-14 20:53:15 -0300

    Integrate user routes with repository

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -1,34 +1,49 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 from starlette.status import (
     HTTP_201_CREATED,
     HTTP_204_NO_CONTENT,
+    HTTP_404_NOT_FOUND,
 )
+
+from app.core.database import get_db
+from app.core.repositories import UsersRepository
+from app.core.schemas import UserCreate, UserRead, UserUpdate
 
 router = APIRouter()
 
 
-@router.post("/users", status_code=HTTP_201_CREATED)
-def create():
-    return "Create user"
+@router.post("/users", response_model=UserRead, status_code=HTTP_201_CREATED)
+def create(user: UserCreate, db: Session = Depends(get_db)):
+    return UsersRepository(db).create(user.dict())
 
 
-@router.get("/users", response_model=List)
-def list():
-    return []
+@router.get("/users", response_model=List[UserRead])
+def list(db: Session = Depends(get_db)):
+    return UsersRepository(db).find_all()
 
 
-@router.get("/users/{user_id}")
-def read(user_id: int):
-    return f"Read user {user_id}"
+@router.get("/users/{user_id}", response_model=UserRead)
+def read(user_id: int, db: Session = Depends(get_db)):
+    try:
+        return UsersRepository(db).find_by_id(user_id)
+    except Exception:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
 
 
-@router.put("/users/{user_id}")
-def update(user_id: int):
-    return f"Update user {user_id}"
+@router.put("/users/{user_id}", response_model=UserRead)
+def update(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
+    try:
+        return UsersRepository(db).update_by_id(user_id, user.dict(exclude_unset=True))
+    except Exception:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
 
 
 @router.delete("/users/{users_id}", status_code=HTTP_204_NO_CONTENT)
-def delete(user_id: int):
-    return f"Delete user {user_id}"
+def delete(user_id: int, db: Session = Depends(get_db)):
+    try:
+        return UsersRepository(db).delete_by_id(user_id)
+    except Exception:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
