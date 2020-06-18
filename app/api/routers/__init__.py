1a923bed9e1b9a165c469eac14fedb942f8c6535
Luiz Lima <umluizlima@gmail.com>
2020-06-17 22:52:14 -0300

    Add token dependency on users routes

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -1,9 +1,13 @@
 from fastapi import Depends
 
+from ..security import token_checker
 from .users import router as users_router
 
 
 def configure(app, settings):
     app.include_router(
-        users_router, tags=["users"], prefix="/api/v1",
+        users_router,
+        tags=["users"],
+        prefix="/api/v1",
+        dependencies=[Depends(token_checker)],
     )
