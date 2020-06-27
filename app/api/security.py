056de8b1ba5c4e2e450daca0ce3ba1f7c9c3800d
umluizlima <umluizlima@gmail.com>
2020-06-27 20:36:19 -0300

    Inject dependencies on TokenChecker class

--- app/api/security.py
+++ app/api/security.py
@@ -1,19 +1,24 @@
-from fastapi import Security, HTTPException
+from fastapi import Depends, Security, HTTPException
 from fastapi.security.http import HTTPAuthorizationCredentials, HTTPBearer
 from starlette.status import HTTP_403_FORBIDDEN
 
 from app.core.services import JWTService
+from app.settings import get_settings, Settings
+
+
+def jwt_service(settings: Settings = Depends(get_settings)):
+    return JWTService(settings)
 
 
 class TokenChecker:
-    scheme = HTTPBearer(scheme_name="JWT")
+    _scheme = HTTPBearer(scheme_name="JWT")
 
-    def __init__(self, token_service: JWTService = JWTService()):
-        self.token_service = token_service
+    def __init__(self, token_service: JWTService = Depends(jwt_service)):
+        self._token_service = token_service
 
-    def __call__(self, header: HTTPAuthorizationCredentials = Security(scheme)):
+    def __call__(self, header: HTTPAuthorizationCredentials = Security(_scheme)):
         try:
-            return self.token_service.verify_token(header.credentials)
+            return self._token_service.verify_token(header.credentials)
         except Exception:
             raise HTTPException(
                 status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
