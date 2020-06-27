f268d8817181ece1a48f0b1e277a18896dbb2a6d
umluizlima <umluizlima@gmail.com>
2020-06-27 20:35:48 -0300

    Update JWTService settings

--- app/core/services/jwt.py
+++ app/core/services/jwt.py
@@ -2,23 +2,23 @@ from typing import Dict
 
 from jwt import decode, encode
 
-from app.settings import settings, Settings
+from app.settings import Settings
 
 
 class JWTService:
-    def __init__(self, settings: Settings = settings):
-        self.settings = settings
+    def __init__(self, settings: Settings):
+        self._settings = settings
 
     def generate_token(self, payload: Dict) -> str:
         return encode(
             payload=payload,
-            key=self.settings.JWT_PRIVATE_KEY,
-            algorithm=self.settings.JWT_ALGORITHM,
+            key=self._settings.JWT_PRIVATE_KEY,
+            algorithm=self._settings.JWT_ALGORITHM,
         ).decode(encoding="utf-8")
 
     def verify_token(self, token: str) -> Dict:
         return decode(
             jwt=token,
-            key=self.settings.JWT_PUBLIC_KEY,
-            algorithms=[self.settings.JWT_ALGORITHM],
+            key=self._settings.JWT_PUBLIC_KEY,
+            algorithms=[self._settings.JWT_ALGORITHM],
         )
