4efbcde6f7c8b9ec368137460dec5eeaf3942e0e
umluizlima <umluizlima@gmail.com>
2020-06-27 20:04:13 -0300

    Update tests

--- tests/unit/routers/conftest.py
+++ tests/unit/routers/conftest.py
@@ -1,10 +1,19 @@
+from unittest.mock import MagicMock
+
 from pytest import fixture
 from starlette.testclient import TestClient
 
-from app.api import api
+from app.api import create_api
+from app.core.database import get_db
+
+
+def mock_get_db():
+    return MagicMock()
 
 
 @fixture
-def client():
+def client(settings):
+    api = create_api(settings)
+    api.dependency_overrides[get_db] = mock_get_db
     client = TestClient(api)
     return client
--- tests/unit/test_settings.py
+++ tests/unit/test_settings.py
@@ -2,32 +2,33 @@ from pydantic import ValidationError
 from pytest import raises
 
 from app.settings import Environment, Settings
+from tests.conftest import get_test_settings
 
 
 def test_api_key_is_set_from_env(monkeypatch):
     expected = "this_is_an_api_key"
     monkeypatch.setenv("API_KEY", expected)
-    assert Settings(_env_file=None).API_KEY == expected
+    assert get_test_settings().API_KEY == expected
 
 
 def test_api_key_has_default_value(monkeypatch):
     monkeypatch.delenv("API_KEY", raising=False)
-    assert isinstance(Settings(_env_file=None).API_KEY, str)
+    assert isinstance(get_test_settings().API_KEY, str)
 
 
 def test_env_is_set_from_env(monkeypatch):
     expected = Environment.PRODUCTION.value
     monkeypatch.setenv("ENV", expected)
-    assert Settings(_env_file=None).ENV == expected
+    assert get_test_settings().ENV == expected
 
 
 def test_env_must_exist(monkeypatch):
     expected = "some-env-that-is-not-mapped"
     monkeypatch.setenv("ENV", expected)
     with raises(ValidationError):
-        Settings(_env_file=None)
+        get_test_settings()
 
 
 def test_env_has_default_value(monkeypatch):
     monkeypatch.delenv("ENV", raising=False)
-    assert isinstance(Settings(_env_file=None).ENV, Environment)
+    assert isinstance(get_test_settings().ENV, Environment)
