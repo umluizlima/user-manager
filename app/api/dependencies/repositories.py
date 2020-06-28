ae8c6e88972158e309e1cd074d2da9946c2efa2b
umluizlima <umluizlima@gmail.com>
2020-06-28 20:21:30 -0300

    Add db_session API dependency

--- app/api/dependencies/repositories.py
+++ app/api/dependencies/repositories.py
@@ -1,9 +1,16 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
-from app.core.database import get_db
+from app.core.database import Database
 from app.core.repositories import UsersRepository
+from app.settings import get_settings, Settings
 
 
-def users_repository(db: Session = Depends(get_db)):
+def db_session(settings: Settings = Depends(get_settings)):
+    db = Database(settings)
+    with db.get_session() as session:
+        yield session
+
+
+def users_repository(db: Session = Depends(db_session)):
     return UsersRepository(db)
