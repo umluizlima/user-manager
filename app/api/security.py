ef9977bfd9fd14ded2ede4bbecbaa75f2cf7d045
umluizlima <umluizlima@gmail.com>
2020-06-28 12:02:42 -0300

    Update security dependencies

--- app/api/security.py
+++ app/api/security.py
@@ -5,24 +5,21 @@ from starlette.status import HTTP_403_FORBIDDEN
 from app.core.services import JWTService
 from app.settings import get_settings, Settings
 
+jwt_scheme = HTTPBearer(scheme_name="JWT")
+
 
 def jwt_service(settings: Settings = Depends(get_settings)):
     return JWTService(settings)
 
 
-class TokenChecker:
-    _scheme = HTTPBearer(scheme_name="JWT")
-
-    def __init__(self, token_service: JWTService = Depends(jwt_service)):
-        self._token_service = token_service
-
-    def __call__(self, header: HTTPAuthorizationCredentials = Security(_scheme)):
-        try:
-            return self._token_service.verify_token(header.credentials)
-        except Exception:
-            raise HTTPException(
-                status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
-            )
-
-
-token_checker = TokenChecker()
+def token_checker(
+    token_service: JWTService = Depends(jwt_service),
+    header: HTTPAuthorizationCredentials = Security(jwt_scheme),
+):
+    try:
+        return token_service.verify_token(header.credentials)
+    except Exception as error:
+        raise error
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
