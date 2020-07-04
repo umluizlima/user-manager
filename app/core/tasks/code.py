ff21f3372c1559a9e375ff5cfb1ed1281f4eff20
umluizlima <umluizlima@gmail.com>
2020-07-04 20:52:15 -0300

    Move access code subject and content to settings

--- app/core/tasks/code.py
+++ app/core/tasks/code.py
@@ -7,13 +7,10 @@ from .producer import TasksProducer
 
 
 class SendCodeProducer(TasksProducer):
-    MESSAGE_SUBJECT = "Access code: {}"
-    MESSAGE_CONTENT = "{} is your code to access user-manager"
-
     def send_code(self, code: str, email: EmailStr):
         message = EmailSchema(
             to=email,
-            subject=SendCodeProducer.MESSAGE_SUBJECT.format(code),
-            content=SendCodeProducer.MESSAGE_CONTENT.format(code),
+            subject=self._settings.ACCESS_CODE_MESSAGE_SUBJECT.format(code),
+            content=self._settings.ACCESS_CODE_MESSAGE_CONTENT.format(code),
         )
         self._producer.send_task(Task.SEND_EMAIL, args=[message.dict()])
