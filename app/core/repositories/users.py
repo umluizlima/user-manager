7b7f1ce2d32e73049d8c67e4f49e1f399b8fb76b
umluizlima <umluizlima@gmail.com>
2020-07-04 22:19:47 -0300

    Add find_by_email method

--- app/core/repositories/users.py
+++ app/core/repositories/users.py
@@ -1,6 +1,19 @@
+from pydantic import EmailStr
+from sqlalchemy.orm.exc import NoResultFound
+
+from ..errors import ResourceNotFoundError
 from ..models import User
 from .base import BaseRepository
 
 
 class UsersRepository(BaseRepository):
     __model__ = User
+
+    def find_by_email(self, email: EmailStr) -> User:
+        try:
+            return self._filter_by_email(email).one()
+        except NoResultFound:
+            raise ResourceNotFoundError
+
+    def _filter_by_email(self, email: EmailStr):
+        return self.db.query(self.__model__).filter(self.__model__.email == email)
