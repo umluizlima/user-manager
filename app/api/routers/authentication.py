ea5b683d00e0d175844bd40669b265debb6c35d8
umluizlima <umluizlima@gmail.com>
2020-10-12 15:49:27 -0300

    Update authentication endpoints

--- app/api/routers/authentication.py
+++ app/api/routers/authentication.py
@@ -10,7 +10,14 @@ from pydantic import EmailStr
 
 from app.core.errors import ResourceNotFoundError
 from app.core.repositories import UsersRepository
-from app.core.schemas import JWTPayload, TokenCreate, Token, UserCreate, UserRead
+from app.core.schemas import (
+    AccessCodeCreate,
+    AccessToken,
+    AccessTokenCreate,
+    JWTPayload,
+    UserCreate,
+    UserRead,
+)
 from app.core.services import CodeService, JWTService
 from app.core.tasks import SendCodeProducer
 
@@ -25,9 +32,9 @@ from ..dependencies import (
 router = APIRouter()
 
 
-@router.post("/authentication", status_code=HTTP_202_ACCEPTED)
-def generate_code(
-    body: TokenCreate,
+@router.post("/authentication/access-code", status_code=HTTP_202_ACCEPTED)
+def generate_access_code(
+    body: AccessCodeCreate,
     users: UsersRepository = Depends(users_repository),
     code_service: CodeService = Depends(code_service),
     producer: SendCodeProducer = Depends(send_code_producer),
@@ -42,31 +49,34 @@ def generate_code(
         raise HTTPException(
             status_code=HTTP_404_NOT_FOUND, detail="User not registered"
         )
-    code = code_service.generate_code(f"user:{user.id}")
+    code = code_service.generate_code(f"access_code:{user.id}")
     producer.send_code(code, user.email)
 
 
-@router.get("/authentication", response_model=Token)
-def retrieve_token(
-    email: EmailStr,
-    code: str,
+@router.post(
+    "/authentication/access-token",
+    response_model=AccessToken,
+    status_code=HTTP_202_ACCEPTED,
+)
+def generate_access_token(
+    body: AccessTokenCreate,
     users: UsersRepository = Depends(users_repository),
     code_service: CodeService = Depends(code_service),
     jwt_service: JWTService = Depends(jwt_service),
 ):
     user = None
     try:
-        user = users.find_by_email(email)
+        user = users.find_by_email(body.email)
     except ResourceNotFoundError:
         raise HTTPException(
             status_code=HTTP_404_NOT_FOUND, detail="User not registered"
         )
-    if not code_service.verify_code(f"user:{user.id}", code):
+    if not code_service.verify_code(f"access_code:{user.id}", body.code):
         raise HTTPException(
             status_code=HTTP_400_BAD_REQUEST, detail="Invalid access code"
         )
     jwt = jwt_service.generate_token(JWTPayload(user_id=user.id))
-    return Token(access_token=jwt)
+    return AccessToken(access_token=jwt)
 
 
 def configure(app, settings):
