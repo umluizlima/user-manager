43bef2225f751ae0704ef59859df300fd96c35fd
umluizlima <umluizlima@gmail.com>
2020-10-31 19:46:05 -0300

    Add type hints to configure method

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -7,7 +7,7 @@ from . import extensions, routers
 def create_api(settings: Settings):
     api = FastAPI(title="user-manager")
 
-    extensions.configure(api, settings)
-    routers.configure(api, settings)
+    for resource in [extensions, routers]:
+        resource.configure(api, settings)
 
     return api
--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -1,6 +1,10 @@
+from fastapi import FastAPI
+
+from app.settings import Settings
+
 from . import authentication, users
 
 
-def configure(app, settings):
+def configure(app: FastAPI, settings: Settings):
     for router in [authentication, users]:
         router.configure(app, settings)
