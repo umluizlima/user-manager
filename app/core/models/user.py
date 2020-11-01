041e51382072fc6fdf5dbf52f7924271710cbd8d
umluizlima <umluizlima@gmail.com>
2020-11-01 00:59:58 -0300

    Add UserRoles enum and map roles column on User model

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,9 +1,22 @@
+from enum import Enum, auto
+
 from sqlalchemy import Column, Text
+from sqlalchemy.types import ARRAY
 
 from .base import BaseModel
+
+
+class AutoName(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+
+class UserRoles(AutoName):
+    ADMIN = auto()
 
 
 class User(BaseModel):
     __tablename__ = "users"
 
     email = Column(Text, nullable=False, unique=True)
+    roles = Column(ARRAY(Text, dimensions=1), default=[])
