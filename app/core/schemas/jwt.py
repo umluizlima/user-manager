a66d0620b087365f934e0291cae6b7276ef2b903
umluizlima <umluizlima@gmail.com>
2020-11-29 22:26:33 -0300

    Add token_type to JWT payload schema

--- app/core/schemas/jwt.py
+++ app/core/schemas/jwt.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from time import time
 from typing import List
 from uuid import uuid4
@@ -7,10 +8,15 @@ from pydantic import BaseModel
 from app.core.models import UserRoles
 
 
+class TokenType(str, Enum):
+    ACCESS = "access"
+
+
 class BaseJWTPayload(BaseModel):
     exp: float
     jti: str = str(uuid4())
     nbf: float = time()
+    token_type: TokenType
 
     @staticmethod
     def calc_exp(seconds_from_now: int = 0) -> float:
@@ -19,4 +25,5 @@ class BaseJWTPayload(BaseModel):
 
 class AccessTokenPayload(BaseJWTPayload):
     roles: List[UserRoles]
+    token_type: TokenType = TokenType.ACCESS
     user_id: int
